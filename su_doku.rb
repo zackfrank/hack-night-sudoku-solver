af164e70138b7ba84511c99c44d496aaf43d3a43
Zack Frank <frank.zack@gmail.com>
2018-11-14 22:52:28 -0600

    Add formatting so arrays are made up of nils and integers; methods to grab cells, rows, columns, and start to get (big) squares (with tests)

--- su_doku.rb
+++ su_doku.rb
@@ -5,11 +5,45 @@ class SuDoku
     @level = level
     @puzzle = []
     get_puzzle
+    format_puzzle
   end
 
   def get_puzzle
     File.readlines( './puzzles/easy_01.txt' ).each do |line|
       puzzle << line.chomp.split('')
+    end
+  end
+
+  def format_puzzle
+    puzzle.map! {|row| row.map! {|char| char == '.' ? nil : char.to_i}}
+  end
+
+  def cell_value(x, y)
+    x -= 1
+    y -= 1
+    puzzle[x][y]
+  end
+
+  def row(x)
+    x -= 1
+    puzzle[x]
+  end
+
+  def column(y)
+    y -= 1
+    col = []
+    puzzle.map {|row| col << row[y]}
+    return col
+  end
+
+  def square(num)
+    case num
+    when 1
+      [
+        puzzle[0][0], puzzle[0][1], puzzle[0][2], 
+        puzzle[1][0], puzzle[1][1], puzzle[1][2], 
+        puzzle[2][0], puzzle[2][1], puzzle[2][2] 
+      ]
     end
   end
 
--- su_doku_spec.rb
+++ su_doku_spec.rb
@@ -1,5 +1,15 @@
 require_relative 'su_doku'
 
+#   .256..49.
+#   7...2....
+#   .84.376..
+#   5.9....3.
+#   1.23.58.9
+#   .3....2.4
+#   ..385.92.
+#   ....9...3
+#   .91..357.
+
 RSpec.describe SuDoku do
   
   it "is level: easy" do
@@ -12,10 +22,35 @@ RSpec.describe SuDoku do
     expect(@sudoku.puzzle.length).to eq(9)
   end
 
-  it "has a puzzle made up of arrays of 9 characters each" do
+  it "converts periods to nil" do
     @sudoku = SuDoku.new(:easy)
-    expect(@sudoku.puzzle[0]).to eq(['.', '2', '5', '6', '.', '.', '4', '9', '.'])
-    expect(@sudoku.puzzle[1]).to eq(['7', '.', '.', '.', '2', '.', '.', '.', '.'])
+    expect(@sudoku.puzzle[0]).to eq([nil, 2, 5, 6, nil, nil, 4, 9, nil])
+    expect(@sudoku.puzzle[1]).to eq([7, nil, nil, nil, 2, nil, nil, nil, nil])
+  end
+
+  it "checks the value of a specific square" do
+    @sudoku = SuDoku.new(:easy)
+    expect(@sudoku.cell_value(1, 1)).to eq(nil)
+    expect(@sudoku.cell_value(3, 5)).to eq(3)
+    expect(@sudoku.cell_value(9, 8)).to eq(7)
+  end
+
+  it "checks a row" do
+    @sudoku = SuDoku.new(:easy)
+    expect(@sudoku.row(1)).to eq([nil, 2, 5, 6, nil, nil, 4, 9, nil])
+    expect(@sudoku.row(3)).to eq([nil, 8, 4, nil, 3, 7, 6, nil, nil])
+  end
+
+  it "checks a column" do
+    @sudoku = SuDoku.new(:easy)
+    expect(@sudoku.column(1)).to eq([nil, 7, nil, 5, 1, nil, nil, nil, nil])
+    expect(@sudoku.column(3)).to eq([5, nil, 4, 9, 2, nil, 3, nil, 1])
+  end
+
+  it "checks a square" do
+    @sudoku = SuDoku.new(:easy)
+    expect(@sudoku.square(1)).to eq([nil, 7, nil, 5, 1, nil, nil, nil, nil])
+    expect(@sudoku.column(3)).to eq([5, nil, 4, 9, 2, nil, 3, nil, 1])
   end
 
 end
